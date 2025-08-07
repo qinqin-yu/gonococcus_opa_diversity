c8b2ca85f31c47a438e3bcaf8a17c09716ebf72c
QinQin Yu <qinqinyu@hsph.harvard.edu>
2025-08-07 10:54:33 -0400

    updated itol colors to accommodate 9-12 opas

--- opa_diversity_snakemake/workflow/scripts/write_itol_num_opa_genes.py
+++ opa_diversity_snakemake/workflow/scripts/write_itol_num_opa_genes.py
@@ -16,16 +16,16 @@ args = get_args()
 df = pd.read_csv(args.opa_num, index_col = 0)
 
 df['strain'] = df['strain'].str.replace('#', '_')
-df.rename({'num_opa_genes':'Number of opa genes'}, axis = 'columns', inplace = True)
+df.rename({'num_opa_genes':'# opa genes'}, axis = 'columns', inplace = True)
                    
-legend = df[['strain', 'Number of opa genes']]
-annotation = 'Number of opa genes'
+legend = df[['strain', '# opa genes']]
+annotation = '# opa genes'
 sample_name = 'strain'
 output_path = args.itol_filename
 
 unique_annotations = np.unique(df[annotation])
 color_palette = 'mako'
-colors = ['#0D0707', '#6A3939', "#AF6E6E", "#DBBDBD"] #"#422424", 
+colors = ["#422424", "#AF6E6E", "#DBBDBD", 'white'] #'#0D0707', '#6A3939',
 # colors = sns.color_palette(color_palette, len(unique_annotations)).as_hex()
 colors_dict = dict(zip(unique_annotations, colors))
 
